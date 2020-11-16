7a2ef4d5411337c5a9e60b357db2f0e9555513e0
Jonathan Queiroz <dev@johnjq.com>
2020-11-15 18:46:00 -0800

    Remove old TODO

--- jbozga-producer.py
+++ jbozga-producer.py
@@ -7,7 +7,6 @@ import logging
 import traceback
 import xml.etree.ElementTree as ET
 
-# TODO: implement lujvo decomposition
 # TODO: properly parse command line arguments, and implement at least "--help"
 # TODO: incorporate uakci's rafsi list
 
